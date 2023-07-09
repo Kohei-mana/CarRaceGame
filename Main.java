7073488fb776c20eb414de4f73e7594dc8d9f21c
Kohei-mana <kohei.manabe@tokyo.supersoftware.co.jp>
2023-07-09 17:44:41 +0900

    Q4のアップデート

--- Main.java
+++ Main.java
@@ -1,25 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         
-        // Car[] cars = new Car[3];
-        // cars[0] = new Honda();
-        // cars[1] = new Nissan();
-        // cars[2] = new Ferrari();
+        Car[] cars = new Car[3];
+        cars[0] = new Honda();
+        cars[1] = new Nissan();
+        cars[2] = new Ferrari();
 
-        // for (Car car : cars) {
-        //     System.out.println(car.name + "の価格：" + car.price);
+        for (Car car : cars) {
+            System.out.println("乗車前");
+            System.out.println(car.name + "の加速：" + car.acceleration);
+            car.ridePeople(3);
+            System.out.println("乗車後");
+            System.out.println(car.name + "の加速：" + car.acceleration);
 
-        // }
+        }
 
-        MakeCars make = new MakeCars();
-        Calc calc = new Calc();
-        
-        make.makeCars("Honda");
-        make.makeCars("Nissan");
-        make.makeCars("Ferrari");
-        System.out.println(make.priceList);
-        System.out.println(calc.calcSumPrice(make.priceList));
-        System.out.println(calc.calcAvgPrice(make.priceList));
-        
     }
 }
--- Nissan.java
+++ Nissan.java
@@ -1,5 +1,7 @@
 class Nissan extends Car {
     Nissan() {
         super("Nissan", 4, 4000000, 4.0);
+        //生産時の欠陥が発生し、加速値の60%しか性能がだせない。
+        this.acceleration *= 0.6;
     }
 }
